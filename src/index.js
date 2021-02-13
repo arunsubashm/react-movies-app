5335fcd877ff878a6e5a71fd1df5cc0711d59b7b
Arun Subash Manickam <arunsubashm@gmail.com>
2021-02-13 16:04:24 +0530

    Header Implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './screen/home/Home';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<div></div>, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
